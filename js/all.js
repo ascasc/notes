9df90d1b8c93947a5935e8961c71a636428b664b
顏章羽 <asc824@gmail.com>
2019-07-04 16:47:39 +0800

    新增一個筆記專案

---
+++ js/all.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    
+    $('.about .about-img').animate({
+      opacity: 1 
+    },
+    5000
+   );
+    $('.about .about-article').animate({
+      opacity: 1
+    },
+    7000
+   );
+});
